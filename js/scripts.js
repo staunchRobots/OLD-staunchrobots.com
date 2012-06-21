343e98c34a50b17edc10a4e7124bb1750bc65a9d
Vladimir <penkinv@gmail.com>
2012-06-21 11:23:46 -0500

    Update

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+jQuery(function($) {
+	$('#gallery_1').anythingSlider({
+		startStopped    : false,
+		stopAtEnd : false,
+		autoPlay: false,
+		hashTags: false
+	});
+	$('#gallery_2').anythingSlider({
+		startStopped    : false,
+		stopAtEnd : false,
+		autoPlay: false,
+		hashTags: false
+	});
+	$('#gallery_3').anythingSlider({
+		startStopped    : false,
+		stopAtEnd : false,
+		autoPlay: false,
+		hashTags: false
+	});
+	$('#gallery_4').anythingSlider({
+		startStopped    : false,
+		stopAtEnd : false,
+		autoPlay: false,
+		hashTags: false
+	});
+});
