4bf226466e055f7acb0916f2229eb43c77026fab
Vladimir <penkinv@gmail.com>
2012-06-26 22:52:31 -0500

    Remake if site with Sinatra!

---
+++ app.rb
@@ -0,0 +1,34 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/activerecord"
+require "sinatra/base"
+require "active_record"
+require "uri"
+require "haml"
+require "bundler/setup"
+require "logger"
+
+# App
+require "./helpers.rb"
+require "./models.rb"
+
+# dbconfig = YAML.load(File.read("config/database.yml"))
+RACK_ENV ||= ENV["RACK_ENV"] || "development"
+PER_PAGE = 6
+# ActiveRecord::Base.establish_connection dbconfig[RACK_ENV]
+# Dir.mkdir('log') if !File.exists?('log') || !File.directory?('log')
+# ActiveRecord::Base.logger = Logger.new(File.open("log/#{RACK_ENV}.log", "a+"))
+
+
+## actions ##
+
+  get '/' do
+    haml :index
+  end
+
+  [:index, :jobs, :learn, :"team-page", :team, :technology, :work].each do |page|
+    get "/#{page}*" do
+      haml page
+    end
+  end
+  
