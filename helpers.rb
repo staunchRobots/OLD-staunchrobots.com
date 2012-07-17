07e051f8cb4341f9e77d5ee73bbacc150f2c6033
Todd Morrill <Todd.Morrill@staunchrobots.com>
2012-07-16 23:55:44 -0500

    UPdated work page and about us page

--- helpers.rb
+++ helpers.rb
@@ -92,5 +92,17 @@ helpers do
       value == selected
     end
   end
+  
+  def obscure_email(email)
+    return email
+    return nil if email.nil? #Don't bother if the parameter is nil.
+    lower = ('a'..'z').to_a
+    upper = ('A'..'Z').to_a
+    email.split('').map { |char|
+        output = lower.index(char) + 97 if lower.include?(char)
+        output = upper.index(char) + 65 if upper.include?(char)
+        output ? "&##{output};" : (char == '@' ? '&#0064;' : char)
+    }.join
+  end
 
 end
